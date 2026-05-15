8c93af764196ac000775799fed0fccdee3112c35
margarita0912 <2202199@ucc.edu.ar>
2026-05-15 15:42:21 -0300

    fix(backend): agregar PrismaModule a ClasificacionesModule

--- apps/backend/src/clasificaciones/clasificaciones.module.ts
+++ apps/backend/src/clasificaciones/clasificaciones.module.ts
@@ -2,8 +2,10 @@ import { Module } from '@nestjs/common'
 import { ClasificacionesService } from './clasificaciones.service'
 import { ClasificacionesController } from './clasificaciones.controller'
 import { RolesGuard } from '../common/guards/roles.guard'
+import { PrismaModule } from '../prisma/prisma.module'
 
 @Module({
+  imports: [PrismaModule],
   providers: [ClasificacionesService, RolesGuard],
   controllers: [ClasificacionesController],
   exports: [ClasificacionesService],
