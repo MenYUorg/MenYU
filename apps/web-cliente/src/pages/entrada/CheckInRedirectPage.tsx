4a772ba34cb1c4f9b66f21fb4ea7216b33adb1b3
margarita0912 <2202199@ucc.edu.ar>
2026-06-07 05:37:04 -0300

    fix(web-cliente): suprimir warning react-hooks/exhaustive-deps en CheckInRedirectPage

--- apps/web-cliente/src/pages/entrada/CheckInRedirectPage.tsx
+++ apps/web-cliente/src/pages/entrada/CheckInRedirectPage.tsx
@@ -29,7 +29,7 @@ export default function CheckInRedirectPage() {
         navigate('/', { replace: true })
       }
     })
-  }, [])
+  }, []) // eslint-disable-line react-hooks/exhaustive-deps
 
   return null
 }
